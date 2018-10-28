bfdead9bf55a8c7a05eb9a1545a8543e30b2b183
yosafatwilly <willychristt@gmail.com>
2018-10-29 02:28:38 +0700

    #

--- backend/src/main/java/com/project/bku/model/BkuMappedSuperclass.java
+++ backend/src/main/java/com/project/bku/model/BkuMappedSuperclass.java
@@ -22,37 +22,37 @@ public abstract class BkuMappedSuperclass extends GenericUserDateAudit implement
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-//	@NotBlank
-//	private Date tanggal;
+	@NotBlank
+	private Date tanggal;
 
 	private String noKode;
 
-//	private String noBukti;
-//
-//	@NotBlank
-//	private String uraian;
+	private String noBukti;
 
-//	private Integer penerimaan;
-//
-//	private Integer pengeluaran;
-//
-//	private Date tanggalPelunasan;
-////
-//	private String kodeAkreditasi;
-//
-//	private String kodeKementrian;
-//
-//	private String kodeBkd;
-//
-//	private String kodeLaporanBos;
-////
-//	private String statusPemeriksa;
-//
-//	private String fileBukti;//tanggalupload_tanggalkegiatan_idsekolah
+	@NotBlank
+	private String uraian;
 
-//	@ManyToOne
-//	@JoinColumn(name = "sekolah_id", nullable = false)
-//	private Sekolah sekolah;
+	private Integer penerimaan;
+
+	private Integer pengeluaran;
+
+	private Date tanggalPelunasan;
+
+	private String kodeAkreditasi;
+
+	private String kodeKementrian;
+
+	private String kodeBkd;
+
+	private String kodeLaporanBos;
+
+	private String statusPemeriksa;
+
+	private String fileBukti;//tanggalupload_tanggalkegiatan_idsekolah
+
+	@ManyToOne
+	@JoinColumn(name = "sekolah_id", nullable = false)
+	private Sekolah sekolah;
 
 	public BkuMappedSuperclass() {
 	}
@@ -65,13 +65,13 @@ public abstract class BkuMappedSuperclass extends GenericUserDateAudit implement
 		this.id = id;
 	}
 
-//	public Date getTanggal() {
-//		return tanggal;
-//	}
-//
-//	public void setTanggal(Date tanggal) {
-//		this.tanggal = tanggal;
-//	}
+	public Date getTanggal() {
+		return tanggal;
+	}
+
+	public void setTanggal(Date tanggal) {
+		this.tanggal = tanggal;
+	}
 
 	public String getNoKode() {
 		return noKode;
@@ -81,100 +81,100 @@ public abstract class BkuMappedSuperclass extends GenericUserDateAudit implement
 		this.noKode = noKode;
 	}
 
-//	public String getNoBukti() {
-//		return noBukti;
-//	}
-//
-//	public void setNoBukti(String noBukti) {
-//		this.noBukti = noBukti;
-//	}
-//
-//	public String getUraian() {
-//		return uraian;
-//	}
-//
-//	public void setUraian(String uraian) {
-//		this.uraian = uraian;
-//	}
-//
-//	public Integer getPenerimaan() {
-//		return penerimaan;
-//	}
-//
-//	public void setPenerimaan(Integer penerimaan) {
-//		this.penerimaan = penerimaan;
-//	}
-//
-//	public Integer getPengeluaran() {
-//		return pengeluaran;
-//	}
-//
-//	public void setPengeluaran(Integer pengeluaran) {
-//		this.pengeluaran = pengeluaran;
-//	}
-//
-//	public Date getTanggalPelunasan() {
-//		return tanggalPelunasan;
-//	}
-//
-//	public void setTanggalPelunasan(Date tanggalPelunasan) {
-//		this.tanggalPelunasan = tanggalPelunasan;
-//	}
-//
-//	public String getKodeAkreditasi() {
-//		return kodeAkreditasi;
-//	}
-//
-//	public void setKodeAkreditasi(String kodeAkreditasi) {
-//		this.kodeAkreditasi = kodeAkreditasi;
-//	}
-//
-//	public String getKodeKementrian() {
-//		return kodeKementrian;
-//	}
-//
-//	public void setKodeKementrian(String kodeKementrian) {
-//		this.kodeKementrian = kodeKementrian;
-//	}
-//
-//	public String getKodeBkd() {
-//		return kodeBkd;
-//	}
-//
-//	public void setKodeBkd(String kodeBkd) {
-//		this.kodeBkd = kodeBkd;
-//	}
+	public String getNoBukti() {
+		return noBukti;
+	}
 
-//	public Sekolah getSekolah() {
-//		return sekolah;
-//	}
-//
-//	public void setSekolah(Sekolah sekolah) {
-//		this.sekolah = sekolah;
-//	}
-//
-//	public String getKodeLaporanBos() {
-//		return kodeLaporanBos;
-//	}
-//
-//	public void setKodeLaporanBos(String kodeLaporanBos) {
-//		this.kodeLaporanBos = kodeLaporanBos;
-//	}
-//
-//	public String getStatusPemeriksa() {
-//		return statusPemeriksa;
-//	}
-//
-//	public void setStatusPemeriksa(String statusPemeriksa) {
-//		this.statusPemeriksa = statusPemeriksa;
-//	}
-//
-//	public String getFileBukti() {
-//		return fileBukti;
-//	}
-//
-//	public void setFileBukti(String fileBukti) {
-//		this.fileBukti = fileBukti;
-//	}
+	public void setNoBukti(String noBukti) {
+		this.noBukti = noBukti;
+	}
+
+	public String getUraian() {
+		return uraian;
+	}
+
+	public void setUraian(String uraian) {
+		this.uraian = uraian;
+	}
+
+	public Integer getPenerimaan() {
+		return penerimaan;
+	}
+
+	public void setPenerimaan(Integer penerimaan) {
+		this.penerimaan = penerimaan;
+	}
+
+	public Integer getPengeluaran() {
+		return pengeluaran;
+	}
+
+	public void setPengeluaran(Integer pengeluaran) {
+		this.pengeluaran = pengeluaran;
+	}
+
+	public Date getTanggalPelunasan() {
+		return tanggalPelunasan;
+	}
+
+	public void setTanggalPelunasan(Date tanggalPelunasan) {
+		this.tanggalPelunasan = tanggalPelunasan;
+	}
+
+	public String getKodeAkreditasi() {
+		return kodeAkreditasi;
+	}
+
+	public void setKodeAkreditasi(String kodeAkreditasi) {
+		this.kodeAkreditasi = kodeAkreditasi;
+	}
+
+	public String getKodeKementrian() {
+		return kodeKementrian;
+	}
+
+	public void setKodeKementrian(String kodeKementrian) {
+		this.kodeKementrian = kodeKementrian;
+	}
+
+	public String getKodeBkd() {
+		return kodeBkd;
+	}
+
+	public void setKodeBkd(String kodeBkd) {
+		this.kodeBkd = kodeBkd;
+	}
+
+	public Sekolah getSekolah() {
+		return sekolah;
+	}
+
+	public void setSekolah(Sekolah sekolah) {
+		this.sekolah = sekolah;
+	}
+
+	public String getKodeLaporanBos() {
+		return kodeLaporanBos;
+	}
+
+	public void setKodeLaporanBos(String kodeLaporanBos) {
+		this.kodeLaporanBos = kodeLaporanBos;
+	}
+
+	public String getStatusPemeriksa() {
+		return statusPemeriksa;
+	}
+
+	public void setStatusPemeriksa(String statusPemeriksa) {
+		this.statusPemeriksa = statusPemeriksa;
+	}
+
+	public String getFileBukti() {
+		return fileBukti;
+	}
+
+	public void setFileBukti(String fileBukti) {
+		this.fileBukti = fileBukti;
+	}
 	
 }
