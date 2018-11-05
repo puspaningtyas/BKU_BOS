b6e63473c59f624c50dcd7628c851a660de17727
yosafatwilly <willychristt@gmail.com>
2018-11-05 19:35:28 +0700

    Define UniqConstraint fileBukti

--- backend/src/main/java/com/project/bku/model/Bku2026.java
+++ backend/src/main/java/com/project/bku/model/Bku2026.java
@@ -2,9 +2,10 @@ package com.project.bku.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table
+@Table(uniqueConstraints =  @UniqueConstraint(columnNames = { "fileBukti" }))
 public class Bku2026 extends BkuMappedSuperclass{
 	
 	private static final long serialVersionUID = 1L;
