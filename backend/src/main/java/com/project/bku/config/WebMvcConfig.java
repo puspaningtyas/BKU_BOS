949a647f0b37ad7ff4530f816098f88f413c0dd0
yosafatwilly <willychristt@gmail.com>
2018-11-05 19:34:44 +0700

    Create Bean Model Mapper

--- backend/src/main/java/com/project/bku/config/WebMvcConfig.java
+++ backend/src/main/java/com/project/bku/config/WebMvcConfig.java
@@ -1,5 +1,7 @@
 package com.project.bku.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,5 +17,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
 				.allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE").maxAge(MAX_AGE_SECS);
 	}
 	
-
+	@Bean
+	public ModelMapper modelMapper() {
+	    return new ModelMapper();
+	}
 }
