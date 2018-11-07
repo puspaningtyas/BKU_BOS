6eb5a0bf415b5021e4059b07fb4d4fc4066e2710
puspaningtyas <>
2018-11-08 05:43:22 +0700

    tambah kelas BKU

---
+++ libExcel/BKUExcel/src/com/bku/helper/SekolahHelper.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bku.helper;
+
+import com.bku.pojos.Sekolah;
+import java.util.List;
+
+/**
+ *
+ * @author user only
+ */
+public class SekolahHelper {
+
+    public SekolahHelper() {
+    }
+    
+    public List<Sekolah> getAllSekolah(){
+        return null;
+    }
+    
+    public Sekolah getSekolah(String npsn){
+        return null;
+    }
+}
