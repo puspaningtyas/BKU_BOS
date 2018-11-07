ac42f6bfe12c89648fa1e83c6ce36473f7a65f6d
user only <puspaningtyas@gmail.com>
2018-11-08 05:43:50 +0700

    tambah kelas BKU

---
+++ libExcel/BKUExcel/src/com/bku/helper/BkuHelper.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bku.helper;
+
+/**
+ *
+ * @author user only
+ */
+public class BkuHelper {
+
+    public BkuHelper() {
+    }
+    
+    public void save(){
+        
+    }
+    
+}
