e7a610dc876175ab1c693d795819e831d836dd7d
user only <puspaningtyas@gmail.com>
2018-11-15 08:55:37 +0700

    update converter

--- libExcel/BKUExcel/src/com/bku/xls/BKUConverter.java
+++ libExcel/BKUExcel/src/com/bku/xls/BKUConverter.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package com.bku.xls;
-
 import java.io.File;
 import java.util.ArrayList;
 
@@ -18,7 +17,12 @@ public class BKUConverter {
     }
     
     public ArrayList<Bku> readExcel(File excel){
-     
+         // Creating a Workbook from an Excel file (.xls or .xlsx)
+//        Workbook workbook = WorkbookFactory.create(excel);
+
+        // Retrieving the number of sheets in the Workbook
+//        System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
+
         return null;
     }
 }
