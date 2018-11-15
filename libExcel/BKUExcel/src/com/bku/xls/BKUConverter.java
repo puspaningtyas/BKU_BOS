40969203b2a6d45a0f20ec61ecdd0e3d69c83216
user only <puspaningtyas@gmail.com>
2018-11-15 09:30:19 +0700

    update converter

--- libExcel/BKUExcel/src/com/bku/xls/BKUConverter.java
+++ libExcel/BKUExcel/src/com/bku/xls/BKUConverter.java
@@ -4,8 +4,15 @@
  * and open the template in the editor.
  */
 package com.bku.xls;
+
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 /**
  *
@@ -15,14 +22,21 @@ public class BKUConverter {
 
     public BKUConverter() {
     }
-    
-    public ArrayList<Bku> readExcel(File excel){
-         // Creating a Workbook from an Excel file (.xls or .xlsx)
-//        Workbook workbook = WorkbookFactory.create(excel);
 
-        // Retrieving the number of sheets in the Workbook
-//        System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
+    public ArrayList<Bku> readExcel(File excel) {
+        ArrayList<Bku> list = null;
+        try {
+            // Creating a Workbook from an Excel file (.xls or .xlsx)
+            Workbook workbook = WorkbookFactory.create(excel);
 
-        return null;
+            // Retrieving the number of sheets in the Workbook
+            System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
+
+        } catch (IOException ex) {
+            Logger.getLogger(BKUConverter.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (EncryptedDocumentException ex) {
+            Logger.getLogger(BKUConverter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
     }
 }
