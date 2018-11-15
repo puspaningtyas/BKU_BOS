27d93cfbde532613f4b881eac49981547c939bd7
user only <puspaningtyas@gmail.com>
2018-11-15 10:42:45 +0700

    update converter

--- libExcel/BKUExcel/src/com/bku/xls/BKUConverter.java
+++ libExcel/BKUExcel/src/com/bku/xls/BKUConverter.java
@@ -8,9 +8,11 @@ package com.bku.xls;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
@@ -20,8 +22,9 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
  * @author user only
  */
 public class BKUConverter {
+
     private long npsn;
-    
+
     public BKUConverter() {
     }
 
@@ -37,14 +40,41 @@ public class BKUConverter {
             Sheet sheet = workbook.getSheetAt(0);
             // get npsn
             //    set to npsn active cell
-            
-            
+            Iterator<Row> iterator = sheet.iterator();
+
         } catch (IOException ex) {
             Logger.getLogger(BKUConverter.class.getName()).log(Level.SEVERE, null, ex);
         } catch (EncryptedDocumentException ex) {
             Logger.getLogger(BKUConverter.class.getName()).log(Level.SEVERE, null, ex);
         }
         return list;
+    }
+
+    /**
+     * Fungsi mengambil data npsn
+     *
+     * @param excel
+     * @return
+     */
+    public long readNpsn(File excel) {
+        long npsn = -1;
+        try {
+            // Creating a Workbook from an Excel file (.xls or .xlsx)
+            Workbook workbook = WorkbookFactory.create(excel);
+            
+            // Retrieving the number of sheets in the Workbook
+            System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
+            // get first sheet
+            Sheet sheet = workbook.getSheetAt(0);
+            // get npsn
+            //    set to npsn active cell
+            Iterator<Row> iterator = sheet.iterator();
+        } catch (IOException ex) {
+            Logger.getLogger(BKUConverter.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (EncryptedDocumentException ex) {
+            Logger.getLogger(BKUConverter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return npsn;
     }
 
     /**
