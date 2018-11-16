5be794ee415a1b57d71219e8857ae591e94d5a4d
yosafatwilly <willychristt@gmail.com>
2018-11-17 00:45:12 +0700

    Commit From Intellij

--- backend/src/main/java/com/project/bku/controller/FileController.java
+++ backend/src/main/java/com/project/bku/controller/FileController.java
@@ -1,50 +1,50 @@
-//package com.project.bku.controller;
-//
-//import com.project.bku.model.DBFile;
-//import com.project.bku.payload.UploadFileResponse;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.core.io.ByteArrayResource;
-//import org.springframework.core.io.Resource;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.http.MediaType;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.multipart.MultipartFile;
-//import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-//import com.project.bku.service.impl.DBFileStorageService;
-//
-//@RestController
-//@RequestMapping("/api/file")
-//public class FileController {
-//
-//    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
-//
-//    @Autowired
-//    private DBFileStorageService DBFileStorageService;
-//
-//    @PostMapping("/upload")
-//    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
-//        DBFile dbFile = DBFileStorageService.storeFile(file);
-//
-//        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-//                .path("/api/file/download/")
-//                .path(dbFile.getId())
-//                .toUriString();
-//
-//        return new UploadFileResponse(dbFile.getFileName(), fileDownloadUri,
-//                file.getContentType(), file.getSize());
-//    }
-//
-//    @GetMapping("/download/{fileId}")
-//    public ResponseEntity<Resource> downloadFile(@PathVariable String fileId) {
-//        // Load file from database
-//        DBFile dbFile = DBFileStorageService.getFile(fileId);
-//
-//        return ResponseEntity.ok()
-//                .contentType(MediaType.parseMediaType(dbFile.getFileType()))
-//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + dbFile.getFileName() + "\"")
-//                .body(new ByteArrayResource(dbFile.getData()));
-//    }
-//}
+package com.project.bku.controller;
+
+import com.project.bku.model.DBFile;
+import com.project.bku.payload.UploadFileResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import com.project.bku.service.impl.DBFileStorageService;
+
+@RestController
+@RequestMapping("/api/file")
+public class FileController {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
+    @Autowired
+    private DBFileStorageService DBFileStorageService;
+
+    @PostMapping("/upload")
+    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
+        DBFile dbFile = DBFileStorageService.storeFile(file);
+
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/file/download/")
+                .path(dbFile.getId())
+                .toUriString();
+
+        return new UploadFileResponse(dbFile.getFileName(), fileDownloadUri,
+                file.getContentType(), file.getSize());
+    }
+
+    @GetMapping("/download/{fileId}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileId) {
+        // Load file from database
+        DBFile dbFile = DBFileStorageService.getFile(fileId);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(dbFile.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + dbFile.getFileName() + "\"")
+                .body(new ByteArrayResource(dbFile.getData()));
+    }
+}
