d8f97c16cc681242545d42e7fc39c144e3e07045
yosafatwilly <willychristt@gmail.com>
2018-11-17 00:38:22 +0700

    Commit From Intellij

---
+++ backend/src/main/java/com/project/bku/repository/DBFileRepository.java
@@ -0,0 +1,10 @@
+package com.project.bku.repository;
+
+import com.project.bku.model.DBFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DBFileRepository extends JpaRepository<DBFile, String> {
+
+}
